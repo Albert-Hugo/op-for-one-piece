8ee813705fafda82c1d81ba460dc9dec1f37a8a8
huangyonghui <idorainful@gmail.com>
2019-08-06 18:09:52 +0800

    [fix] fix cache time error

--- chopper/src/main/java/com/ido/op/chopper/CacheMap.java
+++ chopper/src/main/java/com/ido/op/chopper/CacheMap.java
@@ -74,7 +74,7 @@ public class CacheMap<K, V> {
      * @return
      */
     public V put(K key, V value, long expireTime) {
-        MetaData<V> previousValue = map.put(key, new MetaData<>(value, System.currentTimeMillis(), expireTime));
+        MetaData<V> previousValue = map.put(key, new MetaData<>(value, System.currentTimeMillis()/1000, expireTime));
         return previousValue == null ? null : previousValue.value;
     }
 
